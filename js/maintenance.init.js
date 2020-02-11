ab3576a65c0f99042070a8ed7d9c4df28d0898a1
Halcon28 <jose.valles90@hotmail.com>
2020-02-10 23:59:20 -0600

    pagina en construccion avance

--- js/maintenance.init.js
+++ js/maintenance.init.js
@@ -1,7 +1,12 @@
 // maintenance.init.js
   $(function() {
-    var currentDate = new Date();
-    $('div#clock').countdown(60 * 60 * 1000 + currentDate.valueOf(), function(event) {
+    let remainig_time = localStorage.getItem('__countdown');
+    if (!remainig_time){
+      var currentDate = new Date();
+      remainig_time = currentDate.valueOf();
+    }
+    remainig_time = +remainig_time;
+    $('div#clock').countdown((72 * 60 * 60 * 1000) + remainig_time, function(event) {
       $this = $(this);
       switch(event.type) {
         case "seconds":
@@ -14,5 +19,7 @@
           $this.fadeTo('slow', .5);
           break;
       }
+      remainig_time = remainig_time - 1000;
+      localStorage.setItem('__countdown', remainig_time);
     });
   });
